35aa3c507f981c67116878fac332da80eac0b041
Louise Ven <[louise.venuleth@gmail.com] (mailto:louise.venuleth@gmail.com)>
2023-03-15 13:54:44 +0100

    Basic structure of the website

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>PeopleMix</title>
+</head>
+
+<body>
+    <header>
+        <h1>PeopleMix</h1>
+    </header>
+</body>
+
+</html>
